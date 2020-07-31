76b7cfbc7c9567c213cac98f6d50a1328d8bee08
master <rudolifrudolif@gmail.com>
2020-07-31 20:56:36 +0300

    panno page + pluse + minus + scroll

--- app/js/common.js
+++ app/js/common.js
@@ -9,25 +9,6 @@ $(function() {
     $('nav').toggleClass('nav--active');
   });
 
-  // //-------------------------------анімація цифр---------------------------------------
-  //   var show = true;
-  //   var countbox = ".about-statistics__container";
-  //   $(window).on("scroll load resize", function () {
-  //       if (!show) return false; // Отменяем показ анимации, если она уже была выполнена
-  //       var w_top = $(window).scrollTop(); // Количество пикселей на которое была прокручена страница
-  //       var e_top = $(countbox).offset().top; // Расстояние от блока со счетчиками до верха всего документа
-  //       var w_height = $(window).height(); // Высота окна браузера
-  //       var d_height = $(document).height(); // Высота всего документа
-  //       var e_height = $(countbox).outerHeight(); // Полная высота блока со счетчиками
-  //       if (w_top + 500 >= e_top || w_height + w_top == d_height || e_height + e_top < w_height) {
-  //           $('.about-statistics__item h3').spincrement({
-  //               thousandSeparator: "",
-  //               duration: 2000
-  //           });
-  //           show = false;
-  //       }
-  //   });
-
   //------------------------------cart slider-----------------------------
   var tovarPreviews = new Swiper('.tovar__previews', {
     spaceBetween: 15,
@@ -53,3 +34,105 @@ $(function() {
 
 });
 
+
+
+// ------------------------------panno-button-----------------------------
+  $('.panno__pluse').click(function(event){
+    event.preventDefault();
+    $('.panno__img').css('width', function(index, value){
+        var newWidth = value.replace('px','')*1.5;
+        return newWidth;
+    });
+  });
+
+  $('.panno__minus').click(function(event){
+    event.preventDefault();
+    $('.panno__img').css('width', function(index, value){
+      var newWidth = value.replace('px','')/1.5;
+      return newWidth;
+  });
+  });
+
+// ------------------------------panno-scroll-----------------------------
+  (function (root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        define(['exports'], factory);
+    } else if (typeof exports !== 'undefined') {
+        factory(exports);
+    } else {
+        factory((root.dragscroll = {}));
+    }
+  }(this, function (exports) {
+    var _window = window;
+    var _document = document;
+    var mousemove = 'mousemove';
+    var mouseup = 'mouseup';
+    var mousedown = 'mousedown';
+    var EventListener = 'EventListener';
+    var addEventListener = 'add'+EventListener;
+    var removeEventListener = 'remove'+EventListener;
+    var newScrollX, newScrollY;
+
+    var dragged = [];
+    var reset = function(i, el) {
+        for (i = 0; i < dragged.length;) {
+            el = dragged[i++];
+            el = el.container || el;
+            el[removeEventListener](mousedown, el.md, 0);
+            _window[removeEventListener](mouseup, el.mu, 0);
+            _window[removeEventListener](mousemove, el.mm, 0);
+        }
+
+        // cloning into array since HTMLCollection is updated dynamically
+        dragged = [].slice.call(_document.getElementsByClassName('dragscroll'));
+        for (i = 0; i < dragged.length;) {
+            (function(el, lastClientX, lastClientY, pushed, scroller, cont){
+                (cont = el.container || el)[addEventListener](
+                    mousedown,
+                    cont.md = function(e) {
+                        if (!el.hasAttribute('nochilddrag') ||
+                            _document.elementFromPoint(
+                                e.pageX, e.pageY
+                            ) == cont
+                        ) {
+                            pushed = 1;
+                            lastClientX = e.clientX;
+                            lastClientY = e.clientY;
+
+                            e.preventDefault();
+                        }
+                    }, 0
+                );
+
+                _window[addEventListener](
+                    mouseup, cont.mu = function() {pushed = 0;}, 0
+                );
+
+                _window[addEventListener](
+                    mousemove,
+                    cont.mm = function(e) {
+                        if (pushed) {
+                            (scroller = el.scroller||el).scrollLeft -=
+                                newScrollX = (- lastClientX + (lastClientX=e.clientX));
+                            scroller.scrollTop -=
+                                newScrollY = (- lastClientY + (lastClientY=e.clientY));
+                            if (el == _document.body) {
+                                (scroller = _document.documentElement).scrollLeft -= newScrollX;
+                                scroller.scrollTop -= newScrollY;
+                            }
+                        }
+                    }, 0
+                );
+            })(dragged[i++]);
+        }
+    }
+
+      
+    if (_document.readyState == 'complete') {
+        reset();
+    } else {
+        _window[addEventListener]('load', reset, 0);
+    }
+
+    exports.reset = reset;
+  }));
